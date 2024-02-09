c0701dc413d8c9e8679405ab7be503bc7681e80c
Diptajit Das <diptajitdas66@gmail.com>
2024-02-09 11:38:55 +0530

    Added

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const express=require("express")
-const app=express()
 
-require('dotenv').config();
+require("dotenv").config()
 const Port=process.env.PORT || 3000
 
-app.listen(Port,()=>{
-console.log(`server is running at port ${Port}`)
+
+const express = require("express")
+const { userRouter } = require("./routes/user.route")
+const { productRouter } = require("./routes/post.route")
+const cors = require("cors")
+const { connection } = require("./db")
+
+
+const app = express()
+app.use(cors())
+
+app.use("/api/",userRouter)
+app.use("/api/post",productRouter)
+
+
+
+app.listen(Port,async()=>{
+   try {
+    await connection;
+    console.log(`Server is running at port ${Port}`)
+    console.log("Connected to DB")
+   } catch (error) {
+    console.log("Error in connecting with the server due to -->",error)
+   }
 })
+
+module.exports = app
